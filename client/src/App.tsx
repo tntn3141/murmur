057c198a28a93a5498d2bdc846a7ba441edbb2ae
Nhan <tntn@3141@gmail.com>
2024-01-27 23:55:01 +0700

    baseUrl

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,27 +11,19 @@ import { ChatContext } from "./context/ChatContext";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import axios from "axios";
 
-const isDevMode = import.meta.env.DEV;
-const devBaseUrl = import.meta.env.VITE_DEV_SERVER_URL;
-const prodBaseUrl = import.meta.env.VITE_PROD_SERVER_URL;
-
-axios.defaults.baseURL = isDevMode ? devBaseUrl : prodBaseUrl;
+axios.defaults.baseURL = "https://murmur-chat.fly.dev";
 axios.defaults.withCredentials = true;
 
 function App() {
-  const { combinedNotifications, notifications } = useContext(ChatContext);
+  const { notifications } = useContext(ChatContext);
   const notificationCount = notifications.length;
   return (
     <HelmetProvider>
       <Helmet>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {notificationCount && (
-          <title>{`(${notificationCount}) Murmur`}</title>
-        )}
-        {!notificationCount && (
-          <title>Murmur</title>
-        )}
+        {notificationCount && <title>{`(${notificationCount}) Murmur`}</title>}
+        {!notificationCount && <title>Murmur</title>}
         <link rel="icon" type="image/svg+xml" href="./assets/favicon.ico" />
       </Helmet>
       <NavBar />
